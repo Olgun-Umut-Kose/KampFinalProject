1bfc8d2180d3df7b8f1df8ccf13303d0725cc837
BGOuk <ummut22005@gmail.com>
2021-02-04 02:02:46 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EFProductDal());
+
+            
+            foreach (Product p in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(p.ProductName);
+            }
+
+        }
+    }
+}
