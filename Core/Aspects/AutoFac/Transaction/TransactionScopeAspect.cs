be623f2513e998651f4f5e8a6a787a06c75b8fe3
BGOuk <ummut22005@gmail.com>
2021-03-21 13:30:56 +0300

    Transaction

---
+++ Core/Aspects/AutoFac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Transactions;
+using Castle.DynamicProxy;
+using Core.Utilities.Interception;
+using Core.Utilities.Results;
+
+namespace Core.Aspects.AutoFac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    scope.Complete();
+                }
+                catch (Exception e)
+                {
+                    scope.Dispose();
+                    invocation.ReturnValue = new ErrorResult("Bir sorun ile karşılaşıldı: " + e.Message + "inner: " +
+                                                             e.InnerException.Message);
+                }
+            }
+        }
+    }
+}
